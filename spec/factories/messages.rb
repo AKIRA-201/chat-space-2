a91da4ff980c5d5e53d4ad9f95ef127aaf43a2eb
川島賢士 <chage6221@gmail.com>
2018-02-27 17:12:25 +0900

    messageモデルのテスト

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,8 +2,9 @@ FactoryGirl.define do
   factory :message do
     body Faker::Lorem.sentence
     image File.open("#{Rails.root}/public/images/no_image.jpg")
-    user_id
-    group_id
+    group
+    user
   end
+
 end
 
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,17 @@
-require 'rails-helper'
-  describe Message do
-    describe '#index' do
+require 'rails_helper'
+
+RSpec.describe Message, type: :model do
+  describe '#create' do
+    context 'can save' do
+      it 'is valid with body' do
+        message = build(:message, image: nil)
+        expect(message).to be_valid
+      end
+
+      it 'is valid with image' do
+        expect(build(:message, body: nil)).to be_valid
+      end
+
     end
   end
+end

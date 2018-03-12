0f6d78cf52dee1106a2f8fbf663c79e34087740a
川島賢士 <chage6221@gmail.com>
2018-03-12 17:59:02 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,10 @@
 $(function(){
   function buildHTML(message){
-    console.log(message);
-    console.log(message.image);
-    var html = `<div class="messages-list" id="target">
+    var insertImage = '';
+    if(message.image){
+      insertImage =`<img src=${message.image.url} class="img">`;
+    }
+    var html = `<div class="messages-list" id="target" data-message-id="${message.id}">
                   <div class="messages-list--message">
                     <div class="messages-list--message-name">
                       ${message.user_name}
@@ -13,14 +15,43 @@ $(function(){
                     <div class="messages-list--message-body">
                       ${message.body}
                     </div>
-                    <img src=${message.image.url} class="img">
+                    ${insertImage}
                   </div>
                 </div>`
     return html;
   }
+
+  var interval = setInterval(update, 5000);
+
+  function update(){
+    if($('.messages-list')[0]){
+      var message_id = $('.messages-list:last').data('message-id');
+    } else{
+      var message_id = 0
+    }
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      dataType: 'json',
+      data: {id: message_id}
+    })
+    .done(function(data){
+      console.log(data.messages)
+      data.messages.forEach(function(message){
+        if(message.id > message_id){
+          var html =buildHTML(message);
+          $('.chat-main__body').append(html);
+          $('.chat-main__body').animate({scrollTop:$('#target')[0].scrollHeight});
+        }
+      });
+    })
+    .fail(function(data){
+      alert('自動更新に失敗しました');
+    })
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this);
     var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
@@ -35,7 +66,7 @@ $(function(){
       var html =buildHTML(data);
       $('.chat-main__body').append(html).trigger('create');
       $('.chat-main__footer--form-message').val('')
-      if(data.image.url == null)$('.img').remove();
+      if(data.image == null)$('.img').remove();
       $('.chat-main__footer--form').val('')
       $('.img').val('')
       $('.chat-main__body').animate({scrollTop:$('#target')[0].scrollHeight});
